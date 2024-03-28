27e63a9e2a0a8868a7836176dea06ac576542c9d
spike <zvq@live.com>
2024-03-28 10:59:05 +0800

    update

---
+++ fish/alias.fish
@@ -0,0 +1,21 @@
+alias up="brew update && brew upgrade && brew cu -a -y && brew cleanup && mas upgrade && ~/.config/plum/rime-install iDvel/rime-ice:others/recipes/all_dicts"
+alias icon="fileicon set /Applications/Alacritty.app/ /Users/spkz/dotfiles/macOSicons/iTerm.icns && fileicon set /Applications/Spotify.app /Users/spkz/dotfiles/macOSicons/Spotify.icns && fileicon set /Applications/Figma.app /Users/spkz/dotfiles/macOSicons/Figma.icns && fileicon set /Applications/Visual\ Studio\ Code.app/ /Users/spkz/dotfiles/macOSicons/Code.icns && fileicon set /Applications/Notion.app/ /Users/spkz/dotfiles/macOSicons/Notion.icns && fileicon set /Applications/kitty.app /Users/spkz/dotfiles/macOSicons/kitty.icns"
+
+
+alias bi='brew install'
+alias bu='brew uninstall'
+alias bs='brew search'
+alias bl='brew list'
+alias bbd='brew bundle dump --file=~/dotfiles/homebrew/Brewfile --force'
+
+alias df="df -h"
+alias cp='cp -i'
+alias mv="mv -i"
+alias rm='rm -i'
+
+alias vz="vim ~/.zshrc"
+alias sz="source ~/.zshrc"
+alias gp="cd ~/Projects"
+
+# alias vi="nvim"
+# alias vim="nvim"
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,8 @@
 -- Load other files
--- require ("Spoons.inputMethodSwitcher")
+require ("Spoons.inputMethodSwitcher")
 -- require ("Spoons.appIconSwitcher")
 require ("Spoons.openInEditorLiteIconSwitcher")
 require ("Spoons.auto_hs_reload")
 -- require ("Spoons.auto-reload")
 require ("Spoons.double_cmd_q")
--- require ("Spoons.autoBetterDisplay")
+require ("Spoons.kittyHoldCenter")
