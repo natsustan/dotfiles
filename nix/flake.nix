2c6cf85c53ddc31631ef322bd81f49d126e6512e
spike <zvq@live.com>
2024-03-29 23:42:25 +0800

    Update flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,17 +1,6 @@
 {
   description = "Nix for macOS configuration";
 
-  ##################################################################################################################
-  #
-  # Want to know Nix in details? Looking for a beginner-friendly tutorial?
-  # Check out https://github.com/ryan4yin/nixos-and-flakes-book !
-  #
-  ##################################################################################################################
-
-  # the nixConfig here only affects the flake itself, not the system configuration!
-
-  # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
-  # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
   inputs = {
     # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
@@ -21,11 +10,6 @@
     };
   };
 
-  # The `outputs` function will return all the build results of the flake.
-  # A flake can have many use cases and different types of outputs,
-  # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
-  # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
-  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
   outputs = inputs @ {
     self,
     nixpkgs,
@@ -33,7 +17,7 @@
     ...
   }: let
     # TODO replace with your own username and system
-    username = "macOS";
+    username = "spkz";
     system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
 
     hostname = "${username}-macbook";
