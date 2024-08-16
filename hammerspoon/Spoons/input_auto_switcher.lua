ab806878841e1e3aeb9b8e6214ecb89fee09e253
spike <zvq@live.com>
2024-08-16 15:22:39 +0800

    enable hammerspoon.iconChanger

--- hammerspoon/Spoons/input_auto_switcher.lua
+++ hammerspoon/Spoons/input_auto_switcher.lua
@@ -1,3 +1,5 @@
+-- Automatically switch the input source when switching applications.
+
 -- 自动切换输入法
 local function English()
   -- ABC
@@ -78,26 +80,26 @@ function updateFocusAppInputMethod()
   end
 end
 
--- helper hotkey to figure out the app path and name of current focused window
--- ⌃ + ⌘ +.时会显示应用的路径等信息
-hs.hotkey.bind({ 'control', 'cmd' }, ".", function()
-  local appPath = hs.window.focusedWindow():application():path()
-  local appName = hs.window.focusedWindow():application():name()
-  local imSourceId = hs.keycodes.currentSourceID()
+-- -- helper hotkey to figure out the app path and name of current focused window
+-- -- ⌃ + ⌘ +.时会显示应用的路径等信息
+-- hs.hotkey.bind({ 'control', 'cmd' }, ".", function()
+--   local appPath = hs.window.focusedWindow():application():path()
+--   local appName = hs.window.focusedWindow():application():name()
+--   local imSourceId = hs.keycodes.currentSourceID()
 
-  -- 组合信息字符串
-  local info = "App Path:        " .. appPath
-      .. "\n"
-      .. "App Name:      " .. appName
-      .. "\n"
-      .. "IM Source ID:  " .. imSourceId
+--   -- 组合信息字符串
+--   local info = "App Path:        " .. appPath
+--       .. "\n"
+--       .. "App Name:      " .. appName
+--       .. "\n"
+--       .. "IM Source ID:  " .. imSourceId
 
-  -- 显示信息
-  hs.alert.show(info, 3)
+--   -- 显示信息
+--   hs.alert.show(info, 3)
 
-  -- 将信息复制到剪贴板
-  hs.pasteboard.setContents(info)
-end)
+--   -- 将信息复制到剪贴板
+--   hs.pasteboard.setContents(info)
+-- end)
 
 
 -- Handle cursor focus and application's screen manage.
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,9 +5,9 @@ hs.alert.defaultStyle.textSize = 18                          -- 文字大小
 hs.alert.defaultStyle.radius = 10                            -- 圆角大小
 
 -- Load Spoons files
-require("Spoons.auto_hs_reload")
-require("Spoons.double_cmd_q")
-require("Spoons.input_auto_switcher")
-require ("Spoons.app_launcher")
--- require("Spoons.openInEditorLiteIconSwitcher")
--- require ("Spoons.autoBetterDisplay")
+require ("Spoons.appLauncher")
+require ("Spoons.appInfo")
+require ("Spoons.spoonAutoReload")
+require ("Spoons.doubleQToQuit")
+require ("Spoons.input_auto_switcher")
+require ("Spoons.iconChanger")
