c5af8ae9457d1276f730eb7f14c8736b5350f0f4
spike <zvq@live.com>
2024-10-23 10:08:41 +0800

    update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,6 @@
 -- 设置 hs.alert 的默认样式
 hs.alert.defaultStyle.strokeColor = { white = 0, alpha = 1 } -- 边框颜色
-hs.alert.defaultStyle.textFont = "SF Mono"                   -- 字体
+-- hs.alert.defaultStyle.textFont = "SF Mono"                   -- 字体
 hs.alert.defaultStyle.textSize = 18                          -- 文字大小
 hs.alert.defaultStyle.radius = 10                            -- 圆角大小
 
@@ -10,9 +10,9 @@ require ("Spoons.appLauncher")
 require ("Spoons.autoReload")
 require ("Spoons.doubleQToQuit")
 require ("Spoons.inputSourceSwitcher")
-require ("Spoons.autoQuits")
+-- require ("Spoons.autoQuits")
 -- require ("Spoons.autoBetterDisplay")
-require ("Spoons.kittyKeepCenter")
+-- require ("Spoons.kittyKeepCenter")
 
 hs.loadSpoon("ShiftIt")
 spoon.ShiftIt:bindHotkeys({})
