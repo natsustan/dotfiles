663067eeef6fa63ccb6df23edb3e9565f1754a3f
spike <zvq@live.com>
2026-07-09 08:46:10 +0800

    chore(shell): update local tooling setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 /opt/homebrew/bin/brew shellenv | source
 ~/.local/bin/mise activate fish | source
 starship init fish | source
+zoxide init fish | source
+source "$HOME/.cargo/env.fish"
 
 set -U fish_greeting
 
@@ -23,6 +25,7 @@ alias lt='ls -lhtrF'
 alias l.='ls -lhtrdF .*'
 alias lla='ls -la'
 
+alias z='cd'
 alias cd..='cd ..'
 alias cd...='cd ../..'
 alias cd....='cd ../../..'
@@ -36,11 +39,25 @@ alias reload='source ~/.config/fish/config.fish'
 alias vi='nvim'
 alias vim='nvim'
 
-alias up='brew update && brew upgrade && brew cleanup && mas upgrade && ~/.config/plum/rime-install iDvel/rime-ice:others/recipes/all_dicts'
+alias up='brew update && brew upgrade && brew cu -a -y && brew cleanup && mas upgrade'
 alias ip='ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk \'{print $2}\''
 
 alias icon='fileicon set /Applications/kitty.app ~/dotfiles/macOSicons/kitty.icns && killall Dock'
 
-alias claude='claude  --dangerously-skip-permissions'
+alias claude='claude --dangerously-skip-permissions'
 alias claudetg="claude --channels plugin:telegram@claude-plugins-official  --dangerously-skip-permissions"
 alias codex='codex --dangerously-bypass-approvals-and-sandbox'
+
+alias surge-cli='/Applications/Surge.app/Contents/Applications/surge-cli'
+
+# opencode
+fish_add_path /Users/spike/.opencode/bin
+
+export https_proxy=http://127.0.0.1:6152;export http_proxy=http://127.0.0.1:6152;export all_proxy=socks5://127.0.0.1:6153
+
+
+
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/Users/spike/.local/bin" $PATH
